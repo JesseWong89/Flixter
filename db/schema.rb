a3dbd90b321823ec5a326e1043778467d6f87312
Jesse Wong <imionjay@gmail.com>
2018-07-11 16:59:51 +0000

    Dropped database and migrated database again

--- db/schema.rb
+++ db/schema.rb
@@ -35,8 +35,11 @@ ActiveRecord::Schema.define(version: 20180620161127) do
   end
 
   create_table "sections", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_sections_on_course_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
